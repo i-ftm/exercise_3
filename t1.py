3c3da30d98f29eb96080fb1c712d63b369afaf8c
i-ftm <ftm.nateghi@gmail.com>
2024-08-19 00:08:10 +0330

    first version , complite

--- t1.py
+++ t1.py
@@ -7,7 +7,13 @@ def free_competition(n , times):
     contests.sort(key= lambda x:x[1]) #چون دیر تر تموم شده ینی اخر همه به خاطر همین براساس زمان پایان مرتب میکنیم
     last_end_time = -1
     max_count = 0
+    for start, end in contests:
+        if start >= last_end_time:
+            max_count +=1
+            last_end_time= end
 
-n = int(input('Enter a number: '))
-times = list(map(int, input('enter all times: ').split()))
+    print(max_count)
+
+n = int(input())
+times = list(map(int, input().split()))
 free_competition(n,times)
