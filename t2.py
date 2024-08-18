7598045af2f9d022e633da817cbe3d8093ade345
i-ftm <ftm.nateghi@gmail.com>
2024-08-19 00:04:00 +0330

    first version , not complite

--- t2.py
+++ t2.py
@@ -1,34 +1,36 @@
-class graph():
+class Graph:
     def __init__(self):
         self.graph = {}
 
     def addEdge(self,person,frind):
         if person not in self.graph:
-            self.graph['person'] = []
-        self.graph['person'].append(frind)
+            self.graph[person] = []
+            self.graph[person].append(frind)
 
     def bfs(self,start,goal):
         visited = set()
         queue = [start]
+
         while queue:
             vertex = queue.pop(0)
-            visited.add(vertex)
             if vertex == goal:
                 return 'True'
-            queue.extend(self.graph.get(vertex,[]))
+            if vertex not in visited:
+                visited.add(vertex)
+                queue.extend(self.graph.get(vertex,[]))
         return 'False'
 
 
 
-g = graph()
+g = Graph()
 n = int(input())
 for _ in range(n):
     line = input()
-    person , frinds = line.split('#')
-    frinds_list = frinds.split('-')
+    person, f = line.split('#')
+    frinds_list = f.split('-')
     for frind in frinds_list:
         g.addEdge(person,frind)
 
-start , goal = input().split('-')
-result = g.bfs(start,goal)
+start, goal = input().split('-')
+result = g.bfs(start, goal)
 print(result)
