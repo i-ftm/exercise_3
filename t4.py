ebb2b41beb756da487cc45b98132bcf2e2edd010
i-ftm <ftm.nateghi@gmail.com>
2024-08-18 15:47:49 +0330

    first version , not complite

--- t4.py
+++ t4.py
@@ -1,9 +1,22 @@
+def ckeck_equal(numbers):
+    sum1 = sum(numbers)
+    if sum1 % 2 != 0:
+        return 'No'
+    target = sum1 // 2
+    dp_array = [False] * (target + 1)
+    dp_array[0] = True
+
+    for num in numbers:
+        for j in range(target, num - 1, -1):
+            dp_array[j] = dp_array[j] or dp_array[j - num]
+            print(j)
+    return "Yes" if dp_array[target] else "No"
+
 numbers = list(map(int, input().split()))
-numbers.sort()
-while True:
-    a = [numbers.pop()]
-    if sum(numbers) == sum(a):
-        print('Yes')
-        break
-    print('No')
-    break
+result = ckeck_equal(numbers)
+print(result)
+
+
+
+
+
