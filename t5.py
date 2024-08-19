08a01c66a7a3856027d7403f4133356992288c9e
i-ftm <ftm.nateghi@gmail.com>
2024-08-19 18:33:53 +0330

    complete

--- t5.py
+++ t5.py
@@ -1,6 +1,24 @@
+def division_labor(n):
+    di_list=[]
+    for _ in range(n):
+        di_list.append(int(input()))
+    target = sum(di_list)//3
+
+    a = [False for i in range(target+1)]
+    a[0] = True
+    for d in di_list:
+        for i in range(target, d-1, -1):
+            a[i]= a[i] or a[i-d]
+
+    for i in range(target, -1, -1):
+        if a[i]:
+            d_kambiz = i
+            break
+
+    d_all= d_kambiz * 3
+    difference = abs(sum(di_list)-d_all)
+    return difference
+
 n = int(input())
-d = 0
-for _ in range(n):
-    d += int(input())
-result = d % 3
-print(result)
+difference = division_labor(n)
+print(difference)
